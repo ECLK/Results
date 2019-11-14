2efbb1977ad69df2222808e1723a7f40acbea3e0
Yujith Isura <yujith@Yujiths-MacBook-Pro.local>
2019-11-14 22:48:59 +0530

    add year list

--- src/elections/Constants.js
+++ src/elections/Constants.js
@@ -1,8 +1,8 @@
 export const DISPLAY_PARTY_THRESHOLD = 0.00999;
 export const DISPLAY_MOST_RECENT_COUNT = 5;
 
-export const YEAR_LIST = [2015, 2019];
-export const DEFAULT_YEAR = YEAR_LIST[1];
+export const YEAR_LIST = [1982,1988,1994,1999,2005,2010,2015, 2019];
+export const DEFAULT_YEAR = YEAR_LIST[7];
 
 export const COLOR = {
   PARTY_HSLA: {
