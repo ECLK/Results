b4d4c881391767c4805dcfeac9da2eb87c02dc72
Nilesh Jayanandana <nilesh93.j@gmail.com>
2019-11-17 04:55:48 +0530

    google analytics add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import HomePage from './elections/HomePage.js';
 import './App.css';
+import ReactGA from 'react-ga';
 
 function App() {
   return <HomePage />;
 }
 
+function initializeReactGA() {
+  ReactGA.initialize('UA-152722495-1');
+  ReactGA.pageview('/homepage');
+}
 export default App;
