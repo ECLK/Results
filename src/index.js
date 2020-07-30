6389cfdf977bfcf4d4165cf4bf24813f536079db
YujithIsura <ujithisura@gmail.com>
2020-07-30 09:22:35 +0530

    adding g-analytic code

--- src/index.js
+++ src/index.js
@@ -4,8 +4,24 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const injectGA = () => {
+  if (typeof window == 'undefined') {
+    return;
+  }
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+
+  gtag('config', 'UA-152857980-2');
+};
+
 ReactDOM.render(
   <React.StrictMode>
+    {/* Global site tag (gtag.js) - Google Analytics  */}
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-152857980-2"></script>
+    <script>{injectGA()}</script>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
