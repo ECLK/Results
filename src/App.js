60b1d536e5bd8f6ad7c4fbfc89478a4650fc99de
YujithIsura <ujithisura@gmail.com>
2020-07-30 21:10:27 +0530

    g-analytic code changed

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import ReactGA from 'react-ga';
 import HomePage from './elections/components/HomePage.js';
 
-ReactGA.initialize('UA-152857980-2');
-ReactGA.pageview('/#App');
-
 export default class App extends Component {
+
+  componentDidMount(){
+    ReactGA.initialize('UA-152857980-2');
+    ReactGA.pageview('/');
+  }
   render() {
     return (
       <div className="App" key="App">
