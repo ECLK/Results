0f2c5388869b0e385f943a7effde01f2f7164777
YujithIsura <ujithisura@gmail.com>
2020-07-30 15:33:39 +0530

    updating g-analytic code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import './App.css';
-
+import ReactGA from 'react-ga';
 import HomePage from './elections/components/HomePage.js';
 
+ReactGA.initialize('UA-152857980-2');
+ReactGA.pageview('/');
 export default class App extends Component {
   render() {
     return (
