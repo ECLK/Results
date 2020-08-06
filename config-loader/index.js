64e6fd0a8b1b8c40c1ab83d71ad0baf1c79ceb28
YujithIsura <ujithisura@gmail.com>
2020-08-06 18:23:40 +0530

    fix dateissue

--- config-loader/index.js
+++ config-loader/index.js
@@ -1,12 +1,13 @@
 const axios = require('axios');
+const moment = require('moment')
 
 const fs = require("fs")
 
-const username = process.env.username  
-const pass = process.env.password 
-const targetURL = process.env.targetURL  
-const filename = process.env.filename 
-const interval =  process.env.interval || 20000
+const username = process.env.username
+const pass = process.env.password
+const targetURL = process.env.targetURL
+const filename = process.env.filename
+const interval = process.env.interval || 20000
 
 
 function requestData() {
@@ -19,6 +20,12 @@ function requestData() {
         }
     }).then((data) => {
         console.log("data recieved..")
+        data.data = data.data.map((o) => {
+            return {
+                ...o,
+                timestamp: (moment(o.timestamp).toDate().getTime()) / 1000
+            }
+        });
         writeFile(data.data)
     }).catch((err) => {
         console.error("error fetchhing data", err)
